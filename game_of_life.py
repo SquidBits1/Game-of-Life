1614d3a116246f5479b526363d4fe733dc8cd577
CanaryTheBird <139956912+CanaryTheBird@users.noreply.github.com>
2025-01-14 13:05:21 +0000

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -21,52 +21,42 @@ def update_cell(cell_section: np.array):
     centre = cell_section[1,1]
     neightbours = cell_section
     neightbours[1,1] = 0
-    neightbours.count('1')
-    pass
-
+    c = np.count_nonzero(neightbours == 1)
+    if c == 0:
+        centre = 0
+    if c == 1:
+        centre = 0
+    if c == 2 and centre == 1:
+        centre = 1
+    if c == 2 and centre == 0:
+        centre = 0
+    if c == 3:
+        centre = 1
+    if c >=4:
+        centre = 0
+    
+    return centre
 def display_grid(grid):
     pass
-
-
-
 
 def test_update_grid():
     pass
 
 def test_update_cell():
-    assert(update_cell(np.array[0,0,0],
-                       [0,0,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,0,0],
-                       [0,1,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,0,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,1,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,0,0],
-                       [0,1,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,1,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,0,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,1,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,0,1],
-                       [0,1,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [1,1,1],
-                       [0,1,0])) == 0
+    assert(update_cell(np.array([[0,0,0],[0,0,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,0,0],[0,1,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,0,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,1,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,0,0],[0,1,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,1,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,0,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,1,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,0,1],[0,1,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[1,1,1],[0,1,0]]))) == 0
     
 
 def main():
-    update_cell(np.array([0,1,0],[0,1,0],[0,0,0]))
+    update_cell(np.array([[0,1,0],[0,1,0],[0,0,0]]))
+    test_update_cell()
 
 main()

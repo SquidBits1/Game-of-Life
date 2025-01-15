510bd39f67b5e1a4ca7ece5338a93ff9d458684d
CanaryTheBird <139956912+CanaryTheBird@users.noreply.github.com>
2025-01-15 01:36:14 +0000

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -14,16 +14,35 @@ def generate_grid(length, alive_probability):
     """
     grid = (np.random.random((length,length)) < alive_probability).astype(np.int8)
     return grid
-def update_grid(old_grid, rule=None):
-    pass
+def update_grid(old_grid, length=5,rule=None):
+    new_grid = np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+    cell_section = np.array([[0,0,0],[0,0,0],[0,0,0]])
+    for y in range(len(old_grid)):
+        for x in range(len(old_grid[0])):
+            #create cell section
+            for i in range(3):
+                for j in range(3):
+                    place_y = (x+i-1)%length
+                    place_x = (y+j-1)%length
+                    cell_section[j,i] = old_grid[place_x,place_y]
+            c = update_cell(cell_section)
+            new_grid[y,x] = c
+    return new_grid
+    #acc update now
 
 def update_cell(cell_section: np.array):
     centre = cell_section[1,1]
-    neightbours = cell_section
-    neightbours[1,1] = 0
-    neightbours.count('1')
-    pass
-
+    neighbours = cell_section
+    neighbours[1,1] = 0
+    c = np.count_nonzero(neighbours == 1)
+    if c <= 1:
+        return 0
+    if c == 2:
+        return centre
+    if c == 3:
+        return 1
+    if c >= 4:
+        return 0
 def display_grid(grid):
     pass
 
@@ -31,42 +50,27 @@ def display_grid(grid):
 
 
 def test_update_grid():
+    #assert(update_grid(np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]))) == ([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
     pass
-
 def test_update_cell():
-    assert(update_cell(np.array[0,0,0],
-                       [0,0,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,0,0],
-                       [0,1,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,0,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,1,0],
-                       [0,0,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,0,0],
-                       [0,1,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [0,1,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,0,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,1,0],
-                       [0,1,0])) == 1
-    assert(update_cell(np.array[0,1,0],
-                       [1,0,1],
-                       [0,1,0])) == 0
-    assert(update_cell(np.array[0,1,0],
-                       [1,1,1],
-                       [0,1,0])) == 0
+    assert(update_cell(np.array([[0,0,0],[0,0,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,0,0],[0,1,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,0,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,1,0],[0,0,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,0,0],[0,1,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[0,1,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,0,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,1,0],[0,1,0]]))) == 1
+    assert(update_cell(np.array([[0,1,0],[1,0,1],[0,1,0]]))) == 0
+    assert(update_cell(np.array([[0,1,0],[1,1,1],[0,1,0]]))) == 0
     
-
 def main():
-    update_cell(np.array([0,1,0],[0,1,0],[0,0,0]))
-
+    update_cell(np.array([[0,1,0],[0,1,0],[0,0,0]]))
+    test_update_cell()
+    grid = generate_grid(5,0.1)
+    #update_grid(grid)
+    test = np.array([[0,0,0,0,0],[0,0,1,0,0],[0,1,1,1,0],[0,0,1,0,0],[0,0,0,0,0]])
+    print(test)
+    print(update_grid(test))
+    
 main()
